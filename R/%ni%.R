1b1f5495cd93131de1747635ceb35fa2c53e06d5
kamclean <mcleankaca@gmail.com>
2018-04-12 17:58:41 +0100

    fix %ni%

--- R/%ni%.R
+++ R/%ni%.R
@@ -1,3 +1,5 @@
 # %ni%-------------------------
 # Use: the inverse function of %in% (e.g. selects all not in)
-"%ni%" <- Negate("%in%")
+"%ni%" <- function(x, y) {
+  "%ni%" <- Negate("%in%")
+  x %ni% y}
