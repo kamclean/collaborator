2c3d8ee6aa67e70e8702b8466e4901af43d20dfd
kamclean <mcleankaca@gmail.com>
2018-08-03 14:16:55 +0100

    updated vignettes

--- R/report_auth.R
+++ R/report_auth.R
@@ -3,15 +3,17 @@
 # Documentation
 #' Generate a formatted authorship list.
 #' @description Used to generate a formatted authorship list for all users by group (the group the user belongs to e.g. the centre which they participated at). Optional subdivisions can be created to stratify users and groups. This could be a role (e.g. collaborator and validator) or region/country.
-#' @param df Dataframe of at least 2 columns: "name" and "group".
+#' @param df Dataframe of at least 2 columns: "name".
+#' @param group Column name of a variable in the dataframe by which to group authorship (the default is NULL).
+#' @param subdivision Column name of an additional variable in the dataframe by which to subdivide authorship (the default is NULL).
 #' @param name_sep Character(s) which will separate names within the group (the default is ",").
 #' @param group_brachet Character(s) bracheting the group (the default is "()").
 #' @param group_sep Character(s) which will separate the groups (the default is ";").
-#' @param subdivision Column name of an additional variable in the dataframe by which to subdivide authorship (the default is NULL).
 #' @return Generates a text file ("auth_out.txt").
 
 # Function:
-report_auth <- function(df, name_sep = ",", group_brachet = "()",group_sep = ";", subdivision = NULL){
+report_auth <- function(df, group = NULL, subdivision = NULL,
+                        name_sep = ",", group_brachet = "()",group_sep = ";"){
   require("dplyr")
   require("stringr")
   require("stringi")
@@ -20,8 +22,17 @@ report_auth <- function(df, name_sep = ",", group_brachet = "()",group_sep = ";"
   group_brachet_L = substr(group_brachet, 1, 1)
   group_brachet_R = substr(group_brachet, 2, 2)
 
-  if(is.null(subdivision)==T){
+  if(is.null(group)==TRUE&is.null(subdivision)==TRUE){
     auth_out <- df %>%
+
+      dplyr::summarise(name_list = paste0(paste(c(name), collapse=paste0(name_sep, " ")),"."))
+
+      readr::write_file(auth_out$name_list, path="auth_out.txt")}
+
+  if(is.null(group)==FALSE&is.null(subdivision)==TRUE){
+    auth_out <- df %>%
+      mutate(subdivision = pull(df, group)) %>%
+
       group_by(group) %>%
 
       dplyr::summarise(name_list = paste(c(name), collapse=paste0(name_sep, " "))) %>%
@@ -32,13 +43,28 @@ report_auth <- function(df, name_sep = ",", group_brachet = "()",group_sep = ";"
 
       mutate(auth_out = stringi::stri_replace_last_fixed(auth_out, paste0(group_sep, " "), "."))
 
-    write(auth_out$auth_out, file="auth_out.txt")}
+      readr::write_file(auth_out$auth_out, path="auth_out.txt")}
 
-
-
-  else{
-    auth_out_sd <- df %>%
+  if(is.null(group)==TRUE&is.null(subdivision)==FALSE){
+     auth_out <- df %>%
       mutate(subdivision = pull(df, subdivision)) %>%
+
+      select(subdivision, name) %>%
+
+      group_by(subdivision) %>%
+
+      dplyr::summarise(name_list = paste0(paste(c(name), collapse=paste0(name_sep, " ")),".")) %>%
+
+      mutate(auth_out = paste0(name_list, "\n", sep="")) %>%
+
+      mutate(auth_out_sd = paste0(subdivision, ": ", auth_out))
+
+     write(auth_out$auth_out_sd, file="auth_out.txt")}
+
+  if(is.null(group)==FALSE&is.null(subdivision)==FALSE){
+    auth_out <- df %>%
+      mutate(group = pull(df, group),
+             subdivision = pull(df, subdivision)) %>%
 
       select(subdivision, group, name) %>%
 
@@ -56,8 +82,6 @@ report_auth <- function(df, name_sep = ",", group_brachet = "()",group_sep = ";"
 
       mutate(auth_out_sd = paste0(subdivision, ": ", auth_out))
 
-    write(auth_out_sd$auth_out_sd, file="auth_out.txt")}
+    write(auth_out$auth_out_sd, file="auth_out.txt")}
 
-  visualise_list <- cat(read_file("auth_out.txt"))
-
-  return(visualise_list)}
+  return(cat(read_file("auth_out.txt")))}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-adding a line
-adding a line
+collaborator
+==========
+
+The `collaborator` package provides functions which help facilitate administration of multi-centre research using the [REDCap](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2700030/) (Research Electronic Data Capture) application. These functions have been developed and utilised within several collaborative research projects from [Student Audit and Research in Surgery (STARSurg)](https://www.starsurg.org), [EuroSurg](http://www.eurosurg.org), and [West Midlands Research Collaborative (WMRC)](http://www.wmresearch.org.uk).
+
+Installation and Documentation
+------------------------------
+
+You can install `collaborator` from github with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("kamclean/collaborator")
+```
+
+It is recommended that this package is used together with `tidyverse` packages.
+
+Vignettes
+---------
+
+[Redcap User Management](...)
+
+[Generating Authorship Lists](...)
+
+[Generating Missing Data Reports](...)
+
+[Generating a Simple, Easily-shareable Data Dictionary](...)
