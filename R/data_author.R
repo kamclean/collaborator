cc8817bbda5aa1415f3354fb13729f5d19cef315
kamclean <mcleankaca@gmail.com>
2018-08-03 14:40:45 +0100

    document data_author

---
+++ R/data_author.R
@@ -0,0 +1,14 @@
+#' Example dataset for authorship.
+#'
+#' A dataset containing randomly generated names for demonstrating how to generate an authorship list.
+#'
+#' @format A data frame with 100 rows and 3 variables:
+#' \describe{
+#'   \item{name}{randomly generated last name and initial}
+#'   \item{hospital}{example list of hospitals}
+#'   \item{country}{example list of countries}
+#'   ...
+#' }
+#' @name diamonds
+#' @usage data(diamonds)
+"data_author"
