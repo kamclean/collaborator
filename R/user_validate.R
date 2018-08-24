65138482b0db5afe90b6ec6b54fb507eb7a0f5e3
kamclean <mcleankaca@gmail.com>
2018-08-24 11:40:19 +0100

    update vignette user explore

--- R/user_validate.R
+++ R/user_validate.R
@@ -1,12 +1,12 @@
 # user_validate-------------------------
 # Documentation
 #' Validates users have been allocated correctly
-#' @description Used to check all users have been appropriately (dag_unallocated)and correctly (dag_incorrect) allocated to a DAG, and that all user forms rights have been uploaded correctly. Note: if NA, this will default to view & edit access for all forms.
+#' @description Used to check all users have been appropriately (dag_unallocated) and correctly (dag_incorrect) allocated to a DAG, and that all user forms rights have been uploaded correctly. Note: if NA, this will default to view & edit access for all forms.
 #' @param redcap_project_uri URI (Uniform Resource Identifier) for the REDCap instance.
 #' @param redcap_project_token API (Application Programming Interface) for the REDCap project.
 #' @param users.df Dataframe containing at least 2 columns (username, data_access_group)
 #' @param users_exception Vector of usernames to be excluded from comparison (e.g. users who should have access to the whole dataset (dag = NA) or are associated with multiple DAGs in users.df).
-#' @return Nested dataframe of (i) Dataframe of all users numbered by unique role. (ii) Dataframe of each role with an example user with those user rights.
+#' @return Nested dataframe of (i) Users with NA access rights to forms (ii) Users with an unallocated DAG (iii). Users with an incorrect DAG.
 #' @export
 
 # Function:
