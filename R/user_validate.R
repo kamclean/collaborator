db7d07d68582a4fad0d0e5932d87a07d825646f4
kamclean <mcleankaca@gmail.com>
2018-10-23 14:02:00 +0100

    update vignettes

--- R/user_validate.R
+++ R/user_validate.R
@@ -4,7 +4,7 @@
 #' @description Used to check all users have been appropriately (dag_unallocated) and correctly (dag_incorrect) allocated to a DAG, and that all user forms rights have been uploaded correctly. Note: if NA, this will default to view & edit access for all forms.
 #' @param redcap_project_uri URI (Uniform Resource Identifier) for the REDCap instance.
 #' @param redcap_project_token API (Application Programming Interface) for the REDCap project.
-#' @param users.df Dataframe containing at least 2 columns (username, data_access_group)
+#' @param users.df Optional dataframe containing at least 2 columns (username, data_access_group) used to evaluate incorrect DAG
 #' @param users_exception Vector of usernames to be excluded from comparison (e.g. users who should have access to the whole dataset (dag = NA) or are associated with multiple DAGs in users.df).
 #' @return Nested dataframe of (i) Users with NA access rights to forms (ii) Users with an unallocated DAG (iii). Users with an incorrect DAG.
 #' @importFrom dplyr filter mutate select arrange left_join group_by summarise
@@ -14,7 +14,7 @@
 #' @export
 
 # Function:
-user_validate <- function(redcap_project_uri, redcap_project_token, users.df, users_exception){
+user_validate <- function(redcap_project_uri, redcap_project_token, users.df = "", users_exception = ""){
   '%ni%' <- Negate('%in%')
 
   RCurl::postForm(
@@ -34,6 +34,8 @@ user_validate <- function(redcap_project_uri, redcap_project_token, users.df, us
     dplyr::filter(is.na(data_access_group)) %>%
     dplyr::filter(username %ni% users_exception) -> dag_unallocated
 
+
+if(users.df != ""){
   merge.data.frame(users.df, user_current, by = "username") %>%
     dplyr::select("username" = username,
            "DAG_user_new" = data_access_group.x,
@@ -44,5 +46,10 @@ user_validate <- function(redcap_project_uri, redcap_project_token, users.df, us
 
   check.output <- list("forms_na" = forms_na,
                        "dag_unallocated" = dag_unallocated,
-                       "dag_incorrect" = dag_incorrect)
+                       "dag_incorrect" = dag_incorrect)}else{
+
+                         check.output <- list("forms_na" = forms_na,
+                                              "dag_unallocated" = dag_unallocated)}
+
+
   return(check.output)}
