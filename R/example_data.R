5bf354606cc2c5ad73008e5bde8abf6ea1bf81fe
kamclean <mcleankaca@gmail.com>
2018-11-09 19:22:31 +0000

    add dataset documentation

---
+++ R/example_data.R
@@ -0,0 +1,34 @@
+# Documentation of datasets --------------------------------
+# example_data_dict
+#' @docType data
+#' @title Example dataset of surgical patients
+#' @name example_data_dict
+#' @aliases example_data_dict
+#' @description Example dataset to be used within the \href{https://github.com/kamclean/collaborator/blob/master/vignettes/vignette_data_dict.md}{Generating a Simple, Easily-Shareable Data Dictionary} vignette (all data randomly generated).
+#' @format A data frame with 20 rows and 11 variables.
+#' @usage data(example_data_dict)
+#' @keywords data
+NULL
+
+# example_report_author
+#' @docType data
+#' @title Example dataset of authors
+#' @name example_report_author
+#' @aliases example_report_author
+#' @description Example dataset to be used within the \href{https://github.com/kamclean/collaborator/blob/master/vignettes/vignette_authors.md}{Generating Authorship Lists} vignette (all data randomly generated).
+#' @format A data frame with 100 rows and 3 variables.
+#' @usage data(example_report_author)
+#' @keywords data
+NULL
+
+
+# example_df_user
+#' @docType data
+#' @title Example dataset of REDCap users
+#' @name example_df_user
+#' @aliases example_df_user
+#' @description Example dataset to be used within the \href{https://github.com/kamclean/collaborator/blob/master/vignettes/vignette_user_1_explore.md}{Redcap User Management: 1. Explore Current Users} vignette (all data randomly generated).
+#' @format A data frame with 34 rows and 2 variables.
+#' @usage data(example_df_user)
+#' @keywords data
+NULL
